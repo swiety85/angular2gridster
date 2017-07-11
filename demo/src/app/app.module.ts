571b2c5f720a8b805c549ef0c46118370cab8878
swiety85 <swiety85@gmail.com>
2017-07-11 20:51:03 +0200

    Switch to NG-LIGHTNING
    Add widget header with drag and resize switchers

--- demo/src/app/app.module.ts
+++ demo/src/app/app.module.ts
@@ -3,12 +3,8 @@ import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 import { NoopAnimationsModule } from '@angular/platform-browser/animations';
+import { NglModule } from 'ng-lightning/ng-lightning';
 import { GridsterModule } from './gridster/gridster.module';
-import {
-    MdSidenavModule,
-    MdButtonModule,
-    MdToolbarModule
-} from '@angular/material';
 
 import { AppComponent } from './app.component';
 import { TestComponent } from './test/test.component';
@@ -24,9 +20,7 @@ import { TestComponent } from './test/test.component';
         HttpModule,
         GridsterModule,
         NoopAnimationsModule,
-        MdSidenavModule,
-        MdButtonModule,
-        MdToolbarModule
+        NglModule.forRoot()
     ],
     providers: [],
     bootstrap: [AppComponent]
