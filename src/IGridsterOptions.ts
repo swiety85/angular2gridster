f7f83b56687b7cb4656c8c9d19f6490c4c271ce3
swiety85 <swiety85@gmail.com>
2017-11-22 23:17:53 +0100

    feat(resize): configurable resize handlers
    Fixes #135

--- src/IGridsterOptions.ts
+++ src/IGridsterOptions.ts
@@ -6,6 +6,16 @@ export interface IGridsterOptions {
     dragAndDrop?: boolean;
     itemSelector?: string;
     resizable?: boolean;
+    resizeHandles?: {
+        s?: boolean,
+        e?: boolean,
+        n?: boolean,
+        w?: boolean,
+        se?: boolean,
+        ne?: boolean,
+        sw?: boolean,
+        nw?: boolean
+    };
     shrink?: boolean;
     floating?: boolean;
     responsiveView?: boolean;
