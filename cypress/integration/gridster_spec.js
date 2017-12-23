52e0e3e291100c206f7979c61688431d08511255
swiety85 <swiety85@gmail.com>
2017-12-23 19:23:09 +0100

    feat(e2e): First e2e tests

--- cypress/integration/gridster_spec.js
+++ cypress/integration/gridster_spec.js
@@ -1,18 +1,38 @@
 describe('Gridster', function () {
-    it('.should() - assert that <title> is correct', function () {
-        // https://on.cypress.io/visit
-        cy.visit('http://localhost:4200/')
+    beforeEach(function () {
+        cy.visit('http://localhost:4200/');
+    });
 
-        // Here we've made our first assertion using a '.should()' command.
-        // An assertion is comprised of a chainer, subject, and optional value.
+    it('should exist', function () {
 
         // https://on.cypress.io/should
         // https://on.cypress.io/and
 
-        // https://on.cypress.io/title
-        cy.title().should('include', 'Demo')
-        //   ↲               ↲            ↲
-        // subject        chainer      value
+        cy.get('gridster').should('exist');
     });
+
+    it('should contain 5 items', function () {
+        cy.get('gridster').find('gridster-item').should('have.length', 5);
+    });
+
+    it('should drag item', function () {
+        cy
+            .wait(500)
+            .get('gridster-item:eq(0)')
+            .find('.panel-heading')
+            .trigger('mousedown')
+            // .trigger('mousemove', {clientX: 1, clientY: 1})
+            .trigger('mousemove', {clientX: 50, clientY: 50})
+            .trigger('mousemove', {clientX: 350, clientY: 130})
+            .trigger('mousemove', {clientX: 351, clientY: 131})
+            .trigger('mouseup')
+            .should(function (response) {
+                expect(response.parent().parent())
+                    .to.have.css('transform', 'matrix(1, 0, 0, 1, 250, 0)');
+            });
+    });
+
+
+
 
 });
