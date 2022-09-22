83af8b811c7613560acc7309a19167f71b6e8b92
Rami Jebara <ramijebara@users.noreply.github.com>
2022-09-22 10:04:20 -0700

    Feature/angular 14 (#1)
    * rebuild package-lock.json
    
    * ran ng update
    
    * update packages to angular14
    
    * Adding linting and cleaned up unused packages.
    
    * Added tslint to main project file.
    
    * Updated Angular dependencies in gridster project folder.
    
    Co-authored-by: Carson Logan

--- projects/angular2gridster/src/test.ts
+++ projects/angular2gridster/src/test.ts
@@ -1,13 +1,14 @@
 // This file is required by karma.conf.js and loads recursively all the .spec and framework files
 
-import 'core-js/es7/reflect';
 import 'zone.js';
 import 'zone.js/testing';
-import { getTestBed } from '@angular/core/testing';
+
 import {
   BrowserDynamicTestingModule,
   platformBrowserDynamicTesting
 } from '@angular/platform-browser-dynamic/testing';
+
+import { getTestBed } from '@angular/core/testing';
 
 declare const require: any;
 
